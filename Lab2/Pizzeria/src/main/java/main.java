c88e6c189d9f042f012f8c6af47aae9bf38fe8cf
s27085 <s27085@pjwstk.edu.pl>
2023-10-08 14:54:48 +0200

    zadanie 5

--- Lab2/Pizzeria/src/main/java/main.java
+++ Lab2/Pizzeria/src/main/java/main.java
@@ -1,10 +1,14 @@
+import exception.PizzaNotFoundException;
 import model.Order;
 import model.Pizza;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import service.PizzaService;
 
 import java.util.List;
 
 public class main {
+    private static final Logger logger = LogManager.getLogger();
     public static void main(String[] args){
         Pizza pizza1 = new Pizza("Hawajska", true, 150.24);
         Pizza pizza2 = new Pizza("Capricioza", false, 15.42);
@@ -12,9 +16,13 @@ public class main {
         Pizza pizza4 = new Pizza("Halapeno", true, 54.69);
 
         PizzaService mario = new PizzaService(List.of(pizza1, pizza2, pizza3, pizza4));
-
-        Order order1 = mario.makeOrder(List.of(pizza4, pizza1));
-        Order order2 = mario.makeOrder(List.of(pizza3, pizza4));
-        System.out.println(order1);
+        try {
+            Order order1 = mario.makeOrder(List.of(pizza2, pizza1));
+            Order order2 = mario.makeOrder(List.of(pizza3, pizza4));
+            System.out.println(order1);
+        }
+        catch (PizzaNotFoundException e){
+            logger.error("Tried to fetch unavailable pizza. Aborting");
+        }
     }
 }
